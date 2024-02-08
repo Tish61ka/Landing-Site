8caa2234ded6f886e2ee717b795d53dc581d9108
Tishka <yuriitish@mail.ru>
2024-02-08 22:37:03 +0300

    по просьбе Сани

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 
-    var i = false;
     $('.flex-net > div div').height($('.flex-net>div img').height());
 
     $('.flex-net > div').each(function () {
@@ -14,19 +13,12 @@ $(document).ready(function () {
 
     $('.section-3>div:last-child>div:last-child>div span').click(function () {
         if ($(this).hasClass('active') == false) {
-            $(this).addClass('closed')
-            setTimeout(() => {
-                $(this).addClass('active');
-
-                $(this).next().children().slideToggle(400);
-            }, 510);
-
+            $(this).addClass('closed').addClass('active');
         } else {
             $(this).removeClass('closed');
             $(this).removeClass('active');
-
-            $(this).next().children().slideToggle(400);
         }
+        $(this).next().children().slideToggle(400);
 
     });
 });
