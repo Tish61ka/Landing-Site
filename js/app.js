e933d7efc7d0fc9c70fe870453d01c9978daec28
Tishka <yuriitish@mail.ru>
2024-02-17 12:08:03 +0300

    переделал модалку на фулл jq

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     const cards = document.querySelectorAll('.cost__card')
 
+    //анимациия при наведении на карточки с примерами
+
     $('.flex-net > div div').height($('.flex-net>div img').height());
 
     $('.flex-net > div').each(function () {
@@ -12,23 +14,166 @@ $(document).ready(function () {
         );
     });
 
-    $('.section-3>div:last-child>div:last-child>div span').click(function () {
-        if ($(this).hasClass('active') == false) {
-            $(this).addClass('closed').addClass('active');
-        } else {
-            $(this).removeClass('closed');
-            $(this).removeClass('active');
-        }
-        $(this).next().children().slideToggle(400);
+    //модалка
 
-    });
+    const contentModal = {
+        'work-1': [
+            //картинка в хедере
+            'image/yandex-modal.png',
+            // Заголовок
+            'Яндекс1',
+            //Подзаголовок
+            'Разработка одностраничного сайта для лица компании с функционалом расчета стоимости и обратным звонком',
+            //картинка - 1
+            '/image/Joy.png',
+            //текст - 1
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 2
+            '/image/Joy.png',
+            //текст - 2
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 3
+            '/image/Joy.png',
+            //текст - 3
+            'Текст текст текст текст текст текст текст текст',
+        ],
+        'work-2': [
+            //картинка в хедере
+            'image/yandex-modal.png',
+            // Заголовок
+            'Яндекс2',
+            //Подзаголовок
+            'Разработка одностраничного сайта для лица компании с функционалом расчета стоимости и обратным звонком',
+            //картинка - 1
+            '/image/Joy.png',
+            //текст - 1
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 2
+            '/image/Joy.png',
+            //текст - 2
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 3
+            '/image/Joy.png',
+            //текст - 3
+            'Текст текст текст текст текст текст текст текст',
+        ],
+        'work-3': [
+            //картинка в хедере
+            'image/yandex-modal.png',
+            // Заголовок
+            'Яндекс3',
+            //Подзаголовок
+            'Разработка одностраничного сайта для лица компании с функционалом расчета стоимости и обратным звонком',
+            //картинка - 1
+            '/image/Joy.png',
+            //текст - 1
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 2
+            '/image/Joy.png',
+            //текст - 2
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 3
+            '/image/Joy.png',
+            //текст - 3
+            'Текст текст текст текст текст текст текст текст',
+        ],
+        'work-4': [
+            //картинка в хедере
+            'image/yandex-modal.png',
+            // Заголовок
+            'Яндекс4',
+            //Подзаголовок
+            'Разработка одностраничного сайта для лица компании с функционалом расчета стоимости и обратным звонком',
+            //картинка - 1
+            '/image/Joy.png',
+            //текст - 1
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 2
+            '/image/Joy.png',
+            //текст - 2
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 3
+            '/image/Joy.png',
+            //текст - 3
+            'Текст текст текст текст текст текст текст текст',
+        ],
+        'shablon': [
+            //картинка в хедере
+            'image/yandex-modal.png',
+            // Заголовок
+            'Шаблон',
+            //Подзаголовок
+            'Разработка одностраничного сайта для лица компании с функционалом расчета стоимости и обратным звонком',
+            //картинка - 1
+            '/image/Joy.png',
+            //текст - 1
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 2
+            '/image/Joy.png',
+            //текст - 2
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 3
+            '/image/Joy.png',
+            //текст - 3
+            'Текст текст текст текст текст текст текст текст',
+        ],
+        'under-key': [
+            //картинка в хедере
+            'image/yandex-modal.png',
+            // Заголовок
+            'Под ключ',
+            //Подзаголовок
+            'Разработка одностраничного сайта для лица компании с функционалом расчета стоимости и обратным звонком',
+            //картинка - 1
+            '/image/Joy.png',
+            //текст - 1
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 2
+            '/image/Joy.png',
+            //текст - 2
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 3
+            '/image/Joy.png',
+            //текст - 3
+            'Текст текст текст текст текст текст текст текст',
+        ],
+        'all-in-clusive': [
+            //картинка в хедере
+            'image/yandex-modal.png',
+            // Заголовок
+            'Все включено',
+            //Подзаголовок
+            'Разработка одностраничного сайта для лица компании с функционалом расчета стоимости и обратным звонком',
+            //картинка - 1
+            '/image/Joy.png',
+            //текст - 1
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 2
+            '/image/Joy.png',
+            //текст - 2
+            'Текст текст текст текст текст текст текст текст',
+            //картинка - 3
+            '/image/Joy.png',
+            //текст - 3
+            'Текст текст текст текст текст текст текст текст',
+        ],
+    };
+
     function closeModal() {
         $('.modal__example-container').removeClass('open');
         $('html').css('overflow-y', 'scroll');
     }
     function openModal(className) {
-        const renameClass = '.' + className;
-        $(renameClass).addClass('open');
+        $('.modal__example-container').addClass('open');
+        $('.modal_example-header>img').attr('src', contentModal[className][0]);
+        $('.modal_example-main h1').text(contentModal[className][1]);
+        $('.subtitle').text(contentModal[className][2]);
+        $('.img-modal-1>img').attr('src', contentModal[className][3]);
+        $('.img-modal-1 + p').text(contentModal[className][4]);
+        $('.img-modal-2>img').attr('src', contentModal[className][5]);
+        $('.img-modal-2 + p').text(contentModal[className][6]);
+        $('.img-modal-3>img').attr('src', contentModal[className][7]);
+        $('.img-modal-3 + p').text(contentModal[className][8]);
         $('html').css('overflow-y', 'hidden');
     }
 
@@ -39,6 +184,9 @@ $(document).ready(function () {
     $('.modal_example-main button').click(function () {
         closeModal()
     });
+
+    //Открытие модалки в блоке примеры
+
     $('.flex-net>div').click(function (e) {
         const idAttr = $(this).attr('id');
         openModal(idAttr);
@@ -51,6 +199,20 @@ $(document).ready(function () {
         const hrefAttr = $(this).attr('href');
         openModal(hrefAttr);
         e.preventDefault();
+    });
+
+    //Анимация в блоке faq
+
+
+    $('.section-3>div:last-child>div:last-child>div span').click(function () {
+        if ($(this).hasClass('active') == false) {
+            $(this).addClass('closed').addClass('active');
+        } else {
+            $(this).removeClass('closed');
+            $(this).removeClass('active');
+        }
+        $(this).next().children().slideToggle(400);
+
     });
 
     // Анимация карточек
